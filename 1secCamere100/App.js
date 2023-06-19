7dbc5607ce0a51c677534a4007d5efa86b9d39b7
sw21d046 <112365424+sw21d046@users.noreply.github.com>
2023-06-20 00:28:19 +0800

    style saijruulsan bottomTab and cameraScreen
    6/20

--- 1secCamere100/App.js
+++ 1secCamere100/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from "@react-navigation/native";
+import { Ionicons } from 'react-native-vector-icons';
+import { StyleSheet } from 'react-native';
+
 
 import CameraScreen from './src/screens/cameraScreen'
 import VideoLibraryScreen from './src/screens/VideoLibrary';
@@ -22,13 +25,98 @@ function App() {
 
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen options={{headerShown:false}} name="cameraScreen" component={CameraScreen}/>
-        <Tab.Screen options={{headerShown:false}} name="VideoStackComponent" component={VideoStackComponent} />
+      <Tab.Navigator
+  screenOptions={({ route }) => ({
+    tabBarIcon: ({ focused, color, size }) => {
+      let iconName;
 
-      </Tab.Navigator>
+      if (route.name === 'cameraScreen') {
+        iconName = focused ? 'camera' : 'camera-outline';
+      } else if (route.name === 'VideoStackComponent') {
+        iconName = focused ? 'film' : 'film-outline';
+      }
+
+      return <Ionicons name={iconName} size={size} color={color} style={styles.tabBarIconStyle} />;
+    },
+  })}
+  tabBarOptions={{
+    activeTintColor: 'black',
+    inactiveTintColor: 'gray',
+    style: styles.tabBarStyle,
+  }}
+>
+  <Tab.Screen options={{ headerShown: false }} name="cameraScreen" component={CameraScreen} />
+  <Tab.Screen options={{ headerShown: false }} name="VideoStackComponent" component={VideoStackComponent} />
+</Tab.Navigator>
+
     </NavigationContainer>
   );
 }
+const styles = StyleSheet.create({
+  // container: {
+  //   flex: 1,
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  //   backgroundColor: '#424547',
+  // },
+  // camera: {
+  //   width: '100%',
+  //   height: '70%',
+  // },
+  // controls: {
+  //   flexDirection: 'row',
+  // },
+  // cameraButton: {
+  //   backgroundColor: 'white',
+  //   borderRadius: 65,
+  //   paddingHorizontal: 20,
+  //   paddingVertical: 10,
+  //   marginHorizontal: 10,
+  //   height: 100,
+  //   width: 100,
+  //   marginTop: 10,
+  //   marginLeft: 130,
+  // },
+  // ico1: {
+  //   marginLeft: 80,
+  //   fontSize: 40,
+  //   marginTop: 10,
+  //   color: 'white',
+  // },
+  // buttonText: {
+  //   backgroundColor: '#007AFF',
+  //   borderRadius: 5,
+  //   paddingHorizontal: 5,
+  //   paddingVertical: 5,
+  //   marginHorizontal: 5,
+  //   marginTop: 10,
+  // },
+  // zoomButton: {
+  //   width: 100,
+  //   height: 60,
+  // },
+  // ico2: {
+  //   fontSize: 30,
+  //   color: 'white',
+  //   marginRight: 220,
+  // },
+  // ico3: {
+  //   fontSize: 30,
+  //   color: 'white',
+  // },
+  // ico4: {
+  //   fontSize: 40,
+  //   color: 'white',
+  //   marginTop: 10,
+  // },
+  tabBarStyle: {
+    backgroundColor: '#424547',
+    height: 60,
+  },
+  tabBarIconStyle: {
+    marginTop: 5,
+  },
+});
+
 
 export default App;
--- 1secCamere100/src/screens/cameraScreen.js
+++ 1secCamere100/src/screens/cameraScreen.js
@@ -2,11 +2,14 @@ import React, { useEffect, useState, useRef } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { Camera } from 'expo-camera';
 import * as FileSystem from 'expo-file-system';
-import {Ionicons,AntDesign} from 'react-native-vector-icons';
+import { Ionicons, AntDesign,MaterialCommunityIcons} from 'react-native-vector-icons';
+import { FlashMode } from 'expo-camera/build/Camera.types';
+
 const CameraScreen = () => {
   const [hasPermission, setHasPermission] = useState(null);
   const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
   const [zoom, setZoom] = useState(0);
+  const [flashMode, setFlashMode] = useState(Camera.Constants.FlashMode.off);
   const cameraRef = useRef(null);
 
   useEffect(() => {
@@ -57,6 +60,14 @@ const CameraScreen = () => {
     }
   };
 
+  const toggleFlashlight = () => {
+    setFlashMode(
+      flashMode === Camera.Constants.FlashMode.off
+        ? Camera.Constants.FlashMode.torch
+        : Camera.Constants.FlashMode.off
+    );
+  };
+
   if (hasPermission === null) {
     return <View />;
   } else if (hasPermission === false) {
@@ -65,43 +76,52 @@ const CameraScreen = () => {
 
   return (
     <View style={styles.container}>
-       <View style={{flexDirection:'row',marginBottom:10,}}>
-      <TouchableOpacity
-          onPress={handleZoomIn}
-        >
-          <AntDesign name="pluscircleo" style={styles.ico2}/>
+      <View style={{ flexDirection: 'row', marginBottom: 10 }}>
+        <TouchableOpacity onPress={handleZoomIn}>
+          <AntDesign name="pluscircleo" style={styles.ico2} />
         </TouchableOpacity>
-        <TouchableOpacity
-          
-          onPress={handleZoomOut}
-        >
-          <AntDesign name="minuscircleo" style={styles.ico3}/>
+        <TouchableOpacity onPress={toggleFlashlight}>
+          <MaterialCommunityIcons name="lightning-bolt-outline" style={styles.ico5} />
         </TouchableOpacity>
-     </View>
+        <TouchableOpacity onPress={handleZoomOut}>
+          <AntDesign name="minuscircleo" style={styles.ico3} />
+        </TouchableOpacity>
+      </View>
       <Camera
         style={styles.camera}
         type={cameraType}
         zoom={zoom}
+        flashMode={flashMode}
         ref={cameraRef}
       />
       <View style={styles.controls}>
-        <TouchableOpacity
-          style={styles.cameraButton}
-          onPress={startRecording}
-        >
-          <View style={{height:80, width:80,backgroundColor:'black',borderRadius:65,marginLeft:-10}}>
-          <View style={{height:70, width:70,backgroundColor:'red',borderRadius:65,marginLeft:5,marginTop:5}}></View>
+        <TouchableOpacity style={styles.cameraButton} onPress={startRecording}>
+          <View
+            style={{
+              height: 80,
+              width: 80,
+              backgroundColor: 'black',
+              borderRadius: 65,
+              marginLeft: -10,
+            }}
+          >
+            <View
+              style={{
+                height: 70,
+                width: 70,
+                backgroundColor: 'red',
+                borderRadius: 65,
+                marginLeft: 5,
+                marginTop: 5,
+              }}
+            ></View>
           </View>
         </TouchableOpacity>
-        <TouchableOpacity
-         
-          onPress={toggleCameraType}
-        >
-          <Ionicons name="camera-reverse-outline" style={styles.ico1}/>
+        <TouchableOpacity onPress={toggleCameraType}>
+          <Ionicons name="camera-reverse-outline" style={styles.ico1} />
         </TouchableOpacity>
         
       </View>
-      
     </View>
   );
 };
@@ -111,7 +131,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor:'#424547'
+    backgroundColor: '#424547',
   },
   camera: {
     width: '100%',
@@ -119,9 +139,6 @@ const styles = StyleSheet.create({
   },
   controls: {
     flexDirection: 'row',
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    // marginBottom:10
   },
   cameraButton: {
     backgroundColor: 'white',
@@ -129,22 +146,16 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingVertical: 10,
     marginHorizontal: 10,
-    height:100,
-    width:100,
-    marginTop:10,
-    marginLeft:130,
+    height: 100,
+    width: 100,
+    marginTop: 10,
+    marginLeft: 130,
   },
-  ico1:{
-    // backgroundColor: '#007AFF',
-    // borderRadius: 65,
-    // paddingHorizontal: 0,
-    // paddingVertical: 10,
-    // marginHorizontal: 10,
-    marginLeft:80,
-    fontSize:40,
-    marginTop:10,
-    color:'white',
-    
+  ico1: {
+    marginLeft: 80,
+    fontSize: 40,
+    marginTop: 10,
+    color: 'white',
   },
   buttonText: {
     backgroundColor: '#007AFF',
@@ -152,21 +163,31 @@ const styles = StyleSheet.create({
     paddingHorizontal: 5,
     paddingVertical: 5,
     marginHorizontal: 5,
-    marginTop:10,
+    marginTop: 10,
   },
-  zoomButton:{
-    width:100,
-    height:60
+  zoomButton: {
+    width: 100,
+    height: 60,
   },
-  ico2:{
-   fontSize:30,
-   color:"white",
-   marginRight:220
+  ico2: {
+    fontSize: 30,
+    color: 'white',
+    marginRight: 220,
   },
-  ico3:{
-    fontSize:30,
-    color:"white"
-   }
+  ico3: {
+    fontSize: 30,
+    color: 'white',
+  },
+  ico4: {
+    fontSize: 40,
+    color: 'white',
+    marginTop: 10,
+  },
+  ico5:{
+    fontSize:35,
+    color:'white',
+    marginLeft:-120,
+  }
 });
 
-export default CameraScreen; 
+export default CameraScreen;
